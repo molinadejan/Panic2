b2c72ec4d04e4c3a82185e43765c44d382aeaea7
molinadejan <molinadejan@naver.com>
2021-07-10 01:06:48 +0900

    Add GdiPlus
    GDI+ 추가

--- framework.h
+++ framework.h
@@ -5,7 +5,6 @@
 #pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 // Windows 헤더 파일
 #include <windows.h>
 // C 런타임 헤더 파일입니다.
@@ -13,3 +12,10 @@
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+
+#pragma comment(lib, "msimg32.lib")
+
+// GDI+
+#include <ObjIdl.h>
+#include <gdiplus.h>
+#pragma comment(lib, "Gdiplus.lib")
