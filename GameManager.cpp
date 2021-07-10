86f578ac581f62e0e1f13209b64c4783dc3e1ac4
molinadejan <molinadejan@naver.com>
2021-07-10 17:39:48 +0900

    Fix bugs
    플레이어 이동 시 버그 수정
    
    현재 닫힌 영역으로 나아가는 것 구현

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,5 +1,9 @@
 #include "GameManager.h"
 
+#include "MyUtil.h"
+
+Point ZeroPoint = { 0, 0 };
+
 GameManager::GameManager()
 	:input(new InputManager())
 { }
@@ -39,15 +43,67 @@ void GameManager::Update()
 
 	oldTime = newTime;
 
-	if (input->GetKeyDown())
-		player->MovePlayer(0, 1, opened);
-	else if (input->GetKeyUp())
-		player->MovePlayer(0, -1, opened);
+	////////////////////////
+	// 플레이어 입력 처리 //
+	////////////////////////
 
-	if (input->GetKeyLeft())
-		player->MovePlayer(-1, 0, opened);
-	else if (input->GetKeyRight())
-		player->MovePlayer(1, 0, opened);
+	player->SetIsSpace(input->GetKeySpace());
+
+	// 스페이스바 눌렀을 때
+	if (input->GetKeySpace())
+	{
+		Point oldPos = player->GetPos();
+
+		if (input->GetKeyDown())
+			player->MoveWithSpace(0, 1, opened);
+		if (input->GetKeyUp())
+			player->MoveWithSpace(0, -1, opened);
+		if (input->GetKeyLeft())
+			player->MoveWithSpace(-1, 0, opened);
+		if (input->GetKeyRight())
+			player->MoveWithSpace(1, 0, opened);
+
+		Point newPos = player->GetPos();
+
+		Point newDir = GetDir(newPos - oldPos);
+		Point oldDir = player->GetOldDir();
+		
+		// 처음 닫힌 영역으로 나갈때 path에 추가
+		if (player->GetPathSize() == 0)
+			player->AddPath(oldPos);
+
+		// 새로운 방향이 0 이 아니고 이전 방향과 다르면 path에 추가
+		else if (newDir != ZeroPoint && newDir != oldDir)
+			player->AddPath(oldPos);
+
+		// 새로운 방향이 0, 0 이 아니라면 갱신
+		if(newDir != ZeroPoint)
+			player->SetOldDir(newDir.X, newDir.Y);
+	}
+	// 스페이스바 안눌렀을때
+	else
+	{
+		int dirX = 0, dirY = 0;
+
+		if (input->GetKeyDown())
+			dirY = 1;
+		else if (input->GetKeyUp())
+			dirY = -1;
+
+		if (input->GetKeyRight())
+			dirX = 1;
+		else if (input->GetKeyLeft())
+			dirX = -1;
+
+		if (dirX == 0 && dirY == 0)
+			player->MoveBack();
+		else
+			player->MoveWithoutSpace(dirX, dirY, opened);
+	}
+
+	////////////////////////
+	////////////////////////
+	////////////////////////
 }
 
 void GameManager::DrawCover(Graphics * graphic)
