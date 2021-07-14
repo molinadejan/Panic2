c8b8b5e3f2da4598cf2d522deff91df606d1f872
molinadejan <molinadejan@naver.com>
2021-07-14 16:45:38 +0900

    Add Enemy class

---
+++ Enemy.cpp
@@ -0,0 +1,34 @@
+﻿#include "Enemy.h"
+
+#include "MyUtil.h"
+
+Enemy::Enemy(int _x, int _y, RECT _rect)
+	: pos({ (REAL)_x, (REAL)_y }), size(4), speed(1), rect({ _rect.left + size / 2, _rect.top + size / 2, _rect.right - size / 2, _rect.bottom - size / 2 })
+{
+	dir = GetRandomCircle();
+}
+
+void Enemy::Move()
+{
+	PointF newPos = pos + dir;
+
+	int newPosX = Round(newPos.X);
+	int newPosY = Round(newPos.Y);
+
+	if (!InRange(newPosX, rect.left, rect.right))
+		newPos.X = pos.X;
+
+	if (!InRange(newPosY, rect.top, rect.bottom))
+		newPos.Y = pos.Y;
+
+	pos = newPos;
+}
+
+void Enemy::DrawEnemy(Graphics * graphic)
+{
+	int posX = Round(pos.X);
+	int posY = Round(pos.Y);
+
+	SolidBrush brush(Color(255, 255, 125, 0));
+	graphic->FillRectangle(&brush, posX - size / 2, posY - size / 2, size, size);
+}
---
+++ Enemy.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "framework.h"
+
+#include <vector>
+
+using namespace Gdiplus;
+using std::vector;
+
+#ifndef __ENEMY_H__
+#define __ENEMY_H__
+
+class Enemy
+{
+private:
+	
+	// 위치
+	PointF pos;
+
+	// 방향
+	PointF dir;
+
+	// 크기
+	int size;
+
+	// 속력
+	int speed;
+
+	// 이동 가능한 영역
+	RECT rect;
+
+public:
+
+	Enemy(int _x, int _y, RECT _rect);
+	void Move();
+
+	// 적 그리기
+	void DrawEnemy(Graphics *graphic);
+};
+
+#endif // !__ENEMY_H__
